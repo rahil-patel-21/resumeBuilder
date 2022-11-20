5f6715afbff36c54546bd8db01887f4448ad03df
TechAddict <rahil.geek21@outlook.com>
2022-11-20 15:37:05 +0530

    fonts added

--- lib/ui/templates/template_preview.dart
+++ lib/ui/templates/template_preview.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:resumebuilder/ui/custom_widgets/empty_box.dart';
+import 'package:resumebuilder/utils/app_theme.dart';
 import 'package:resumebuilder/utils/size_config.dart';
 
 class TemplatePreviewUI extends StatelessWidget {
@@ -22,15 +23,7 @@ class TemplatePreviewUI extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     _mainComponent(),
-                    Container(
-                      decoration: BoxDecoration(
-                          color: Color(0xFF092A4C),
-                          borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(6),
-                              bottomRight: Radius.circular(6))),
-                      width: SizeConfig.screenWidth * 0.35,
-                      height: SizeConfig.screenHeight * 1.2,
-                    )
+                    _sideComponent(),
                   ],
                 )
               ],
@@ -42,50 +35,102 @@ class TemplatePreviewUI extends StatelessWidget {
   Widget _mainComponent() {
     try {
       return Padding(
-        padding: const EdgeInsets.only(left: 50, right: 30),
+        padding: const EdgeInsets.only(left: 70, right: 30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 100),
             Text('Rahil Patel',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
-            Text('SOFTWARE DEVELOPER', style: TextStyle(fontSize: 10)),
-            SizedBox(height: 40),
-            Text('Profile',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
-            Container(
-              width: 500,
-              child: Text(
-                """Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting. Experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.
-            """,
-                textAlign: TextAlign.start,
-                style: TextStyle(height: 1.5),
-              ),
+                style: TextStyle(fontSize: 30, fontFamily: AppTheme.fNotoSB)),
+            Text(
+              'SOFTWARE DEVELOPER',
+              style: TextStyle(
+                  fontSize: 10,
+                  letterSpacing: 1.2,
+                  fontFamily: AppTheme.fSairaM),
             ),
-            Text('Profile',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
-            Container(
-              width: 500,
-              child: Text(
-                """Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting. Experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.
-            """,
-                textAlign: TextAlign.start,
-                style: TextStyle(height: 1.5),
-              ),
-            ),
-            Text('Profile',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
-            Container(
-              width: 500,
-              child: Text(
-                """Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting. Experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.
-            """,
-                textAlign: TextAlign.start,
-                style: TextStyle(height: 1.5),
-              ),
-            ),
+            _profileSummary(),
+            _employmentSummary()
           ],
         ),
+      );
+    } catch (error) {
+      return EmptyBox();
+    }
+  }
+
+  Widget _sideComponent() {
+    try {
+      return Container(
+        decoration: BoxDecoration(
+            color: Color(0xFF092A4C),
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(6), bottomRight: Radius.circular(6))),
+        width: SizeConfig.screenWidth * 0.35,
+        height: SizeConfig.screenHeight * 1.2,
+        child: Padding(
+          padding: EdgeInsets.only(left: 50),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(height: 200),
+            Text('Details', style: TextStyle(color: Colors.white)),
+            Text('Ahmedabad', style: TextStyle(color: Colors.white)),
+            Text('India', style: TextStyle(color: Colors.white)),
+            Text('+91 1234567890', style: TextStyle(color: Colors.white)),
+          ]),
+        ),
+      );
+    } catch (error) {
+      return EmptyBox();
+    }
+  }
+
+  Widget _profileSummary() {
+    try {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 40),
+          Text('Profile',
+              style: TextStyle(fontFamily: AppTheme.fMontMI, fontSize: 22)),
+          Container(
+            width: 510,
+            child: Text(
+              """Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting. Experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.
+            """,
+              textAlign: TextAlign.start,
+              style: TextStyle(height: 1.5, fontFamily: AppTheme.fMontMI),
+            ),
+          ),
+        ],
+      );
+    } catch (error) {
+      return EmptyBox();
+    }
+  }
+
+  Widget _employmentSummary() {
+    try {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 40),
+          Text('Employment History',
+              style: TextStyle(fontFamily: AppTheme.fMontMI, fontSize: 22)),
+          Text('Team Lead, Lenditt Innovations, Ahmedabad',
+              style: TextStyle(fontFamily: AppTheme.fMontMI, fontSize: 16)),
+          Text('OCT 2021 - PRESENT',
+              style: TextStyle(fontFamily: AppTheme.fSairaM, fontSize: 12)),
+          Container(
+            width: 510,
+            child: Text(
+              """Experienced Web Developer adept in all stages of advanced web development. Knowledgeable in user interface, testing, and debugging processes. Bringing forth expertise in design, installation, testing and maintenance of web systems. Equipped with a diverse and promising skill-set. Proficient in an assortment of technologies, including Java, ASP.NET, C#, IIS, Tomcat, and Microsoft SQL Server. Able to effectively self-manage during independent projects, as well as collaborate in a team setting. Experienced Software Developer adept in bringing forth expertise in design, installation, testing and maintenance of software systems.
+            """,
+              textAlign: TextAlign.start,
+              style: TextStyle(height: 1.5, fontFamily: AppTheme.fMontMI),
+            ),
+          ),
+        ],
       );
     } catch (error) {
       return EmptyBox();
---
+++ lib/utils/app_theme.dart
@@ -0,0 +1,5 @@
+class AppTheme {
+  static String fMontMI = 'Montserrat-Medium-Italic';
+  static String fNotoSB = 'NotoSans-SemiBold';
+  static String fSairaM = 'Saira-Medium';
+}
